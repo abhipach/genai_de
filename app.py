38392770ae0061d7a75cdbe44a50802749a4e0d8
ABHISHEK PACHAURI <40334999+abhipach@users.noreply.github.com>
2024-03-19 22:50:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if "chat_session" not in st.session_state:
 
 
 # Display the chatbot's title on the page
-st.title(" LEX financial Guru")
+st.title("GenAI for DataEngineering Task")
 
 # Display the chat history
 for message in st.session_state.chat_session.history:
